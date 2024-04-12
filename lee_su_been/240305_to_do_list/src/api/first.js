37046041ee6fa23c59642f8a09fac020ff29bb97
어승준 <eoseungjun@eoseungjun-ui-MacBookPro.local>
2024-04-12 17:22:25 +0900

    .

--- lee_su_been/240305_to_do_list/src/api/first.js
+++
@@ -1,9 +0,0 @@
-import index from './index';
-
-export async function RequestToken(){
-    return index.get('/url')
-}
-
-export async function GetToken(code, state){
-    return index.get(`/token?code=${code}&state=${state}`)
-}
