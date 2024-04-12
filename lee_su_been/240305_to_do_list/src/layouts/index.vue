f287f5040e6b3fd2f8c59a1b8fd978ad80143dae
수빈 <supotato@subin-ui-MacBookPro.local>
2024-04-12 18:22:57 +0900

    .

---
+++ lee_su_been/240305_to_do_list/src/layouts/index.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+q-layout(view="1Hh Lor lFf")
+    TodoHeader
+    q-page-container
+        router-view
+    
+
+</template>
+
+<script>
+import TodoHeader from '../components/TodoHeader.vue';
+export default {
+components: {
+    TodoHeader
+}
+}
+</script>
+
+<style lang="stylus">
+
+</style>
